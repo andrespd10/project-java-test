70d91c47e60f02ff409054a7806373d4ea7af072
Andres Daza <felipedazasernadim@gmail.com>
2025-05-12 15:35:31 -0500

    feat: estructura basica del projecto Java

---
+++ primera_clase/Test/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static int sumar(int a, int b) {
+        return a + b;
+    }
+    public static void main(String[] args) throws Exception{
+        String nombre = "Andres";
+        System.out.println("Hello, World!");
+        System.out.println("Proyecto prueba A");
+        System.out.println("Mi nombre es: " + nombre);
+        System.out.println("La suma es: " + sumar(5, 6));
+    }
+}
---
+++ segunda_clase/segunda_c/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        byte b = 100;
+        int myint = 1000;
+        long mylong = myint;
+        String nombre = "Andres";
+        System.out.println("Hello, World!");
+        System.out.println("La variable es: " + myint);
+        System.out.println("Tu nombre es: " + nombre);
+    }
+}
