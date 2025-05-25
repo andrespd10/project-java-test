01ca62795d84b400fc4058cbe059630999c81e88
Andres Daza <felipedazasernadim@gmail.com>
2025-05-25 18:15:21 -0500

    feat:Trabajar con cadenas

---
+++ cadenas/src/cadenasApp.java
@@ -0,0 +1,9 @@
+public class cadenasApp {
+    public static void main(String[] args) throws Exception {
+        String nombre = "Andres";
+
+        System.out.println("Buenas");
+        System.out.println("Tu nombre es: " + nombre);
+        
+    }
+}
