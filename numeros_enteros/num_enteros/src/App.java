ea888b4a22e459c7700fd54db6c8b317ffcf8242
Andres Daza <felipedazasernadim@gmail.com>
2025-05-25 18:13:07 -0500

    feat: Trabajar con números enteros

---
+++ numeros_enteros/num_enteros/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int primer_numero = 10;
+        int segundo_numero = 20;
+        int total;
+
+        total = primer_numero + segundo_numero;
+
+        System.out.println("Primer número entero: " + primer_numero);
+        System.out.println("Segundo número entero: " + segundo_numero);
+        System.out.println("El total de la suma es: " + total);
+        
+    }
+}
