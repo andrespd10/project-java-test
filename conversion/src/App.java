76d64585b1a64aa128ede455ae9ff84f7d3d3de0
Andres Daza <felipedazasernadim@gmail.com>
2025-05-25 18:16:58 -0500

    feat: cálculo y conversión Fahrenheit a Celsius

---
+++ conversion/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public class FahrenheitToCelsius {
+    public static void main(String[] args) throws Exception {
+        double fahrenheit = 98.6;
+        double celsius = (fahrenheit - 32) * 5 / 9;
+
+        System.out.println(fahrenheit + " °F equivalen a " + celsius + " °C");
+    }
+}
+}
